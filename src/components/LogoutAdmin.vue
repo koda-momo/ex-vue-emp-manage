d95654e4e1deb747a7ba250da2bb915479d66868
koda-momo <momoko.kodama@rakus-partners.co.jp>
2021-11-09 11:35:04 +0900

    headerのリンクを修正しました

---
+++ src/components/LogoutAdmin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Logout">
+    <!-- 始め -->
+    <!-- 終わり -->
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class extends Vue {
+  // 始め
+  // 終わり
+}
+</script>
+
+<style></style>
